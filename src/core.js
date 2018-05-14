5d0ac8fab477fb37e03fdcdfa7500081a5ca70ca
Georgie Santiago <Georgie@Georgies-IMac.local>
2018-05-14 12:14:15 -0400

    Make Players and Enemies with ease

--- src/core.js
+++ src/core.js
@@ -33,11 +33,11 @@ var Dust = {
 	 *	@param String resource[index]
 	 *	@return PIXI.Sprite player 
 	 * */
-	Player: function(spritename , position = {0,0})
+	Player: function(spritename , position = {x: 0, y: 0})
 	{
 		var player = new Sprite(id[spritename]);
 		player.x = 68;
-		player.y = gameScene.height / 2 - explorer.height / 2;
+		player.y = gameScene.height / 2 - player.height / 2;
 		player.vx = 0;
 		player.vy = 0;
 		gameScene.addChild(player);
@@ -48,15 +48,17 @@ var Dust = {
 	 *	@param String spritename, Object position, Array options
 	 *	@return PIXI.Sprite object
 	 * */
-	Objects: function(spritename,position={x: 0, y:; 0},options=[])
+	Objects: function(spritename,position={x: 0, y: 0},options={})
 	{
 		var object = new Sprite(id[spritename]);
 		object.position.set(position.x , position.y)
-		if(options.length > 0)
-		{
-			console.log(options);
-		}
-		
+			if(options.length > 0)
+			{
+				object = $.extend({}, object,options);	
+			}
+
+		gameScene.addChild(object);
+
 		return object;
 	},
 
@@ -65,9 +67,56 @@ var Dust = {
 		return false;
 	},
 
-	Enemy: function()
+	Enemy: function(count , name , settings)
 	{
-		return false;
+		let numberOfBlobs = count,
+		spacing = 48,
+		xOffset = 150,
+		speed = 2,
+		direction = 1;
+
+		//An array to store all the blob monsters
+		blobs = [];
+
+		//Make as many blobs as there are `numberOfBlobs`
+		for (let i = 0; i < numberOfBlobs; i++) {
+
+			//Make a blob
+			let blob = new Sprite(id[name]);
+
+			//Space each blob horizontally according to the `spacing` value.
+			//`xOffset` determines the point from the left of the screen
+			//at which the first blob should be added
+			let x = spacing * i + xOffset;
+
+			//Give the blob a random y position
+			let y = randomInt(0, app.stage.height - blob.height);
+
+			//Set the blob's position
+			blob.x = x;
+			blob.y = y;
+
+			//Set the blob's vertical velocity. `direction` will be either `1` or
+			//`-1`. `1` means the enemy will move down and `-1` means the blob will
+			//move up. Multiplying `direction` by `speed` determines the blob's
+			//vertical direction
+			blob.vy = speed * direction;
+
+			//Reverse the direction for the next blob
+			direction *= -1;
+
+			if(settings.length > 1) {
+				$.extend({},blobs,settings);
+			}
+			//Push the blob into the `blobs` array
+			blobs.push(blob);
+
+			//Add the blob to the `gameScene`
+			gameScene.addChild(blob);
+
+		}
+
+		return blobs;
 	},
 
 	World: function()
